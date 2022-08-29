96e3f28db78c513189130914baee2953db142a98
Chris Baum <Christopher.Baum@valpo.edu>
2022-08-29 18:50:32 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,10 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch(action.type) {
+    case "ADD_TODO":
+        console.log("reducer received this action:", action);
+    
+    default: return state
+  }
 }
